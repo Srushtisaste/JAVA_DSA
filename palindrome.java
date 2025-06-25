415d505fedd71374f0874407a839b728adef221a
srushti saste <srushti.saste20@gmail.com>
2025-06-25 13:37:40 +0530

    Check if the string is palindrome or not...

---
+++ palindrome.java
@@ -0,0 +1,19 @@
+public class palindrome{
+
+    public static boolean IsPalindrome(String str){
+        int n = str.length();
+          for(int i =0; i<= n/2 ; i++){
+             if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+             }
+            
+          } 
+           return true;
+          
+    }
+     
+    public static void main (String []args){
+        String str = "racecar";
+        System.out.print(IsPalindrome(str));
+    }
+}
