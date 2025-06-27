a010c20faf0fa3dd29645d89382be27a2a470ef5
srushti saste <srushti.saste20@gmail.com>
2025-06-27 14:37:55 +0530

    Using bitwise operator...

---
+++ BitwiseOp.java
@@ -0,0 +1,21 @@
+public class BitwiseOp{
+    public static void main (String [] args){
+        //Binary AND &
+        System.out.println(5&6);
+
+         //Binary OR |
+        System.out.println(5|6);
+
+         //Binary XOR ^
+        System.out.println(5^6);
+
+         //Binary 1's complement ~
+        System.out.println(~5);
+        
+         //Binary left shift <<
+        System.out.println(5<<6);
+
+          //Binary right shift>>
+        System.out.println(5>>6);
+    }
+}
